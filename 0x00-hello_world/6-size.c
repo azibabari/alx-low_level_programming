c6f022e2fdfe73c4c867fd8f110db3cb94e0a8bd
Kana Azibabari Lagalo <kanaazibabari@gmail.com>
2023-03-20 11:44:41 -0700

    assembler

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i; 
+char c;	
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+return (0);
+}
