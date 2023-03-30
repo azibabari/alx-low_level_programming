4d8fa8d73819f6d7990fed1ce62a37f88ba8935a
Kana Azibabari Lagalo <kanaazibabari@gmail.com>
2023-03-30 05:15:22 -0700

    concatenate two strings 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: holds the number of bytes
+ *
+ * Return: Always 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+char *result = dest; /* keep track of the start of the destination string*/
+/* find the end of the destination string*/
+while (*dest != '\0')
+{
+dest++;
+}   
+/* copy at most n bytes from the source string to the end of the destination string*/
+while (n > 0 && *src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+n--;
+}    
+*dest = '\0'; /* add a terminating null byte to the end of the result string*/  
+return result; /* return a pointer to the resulting string*/
+}
