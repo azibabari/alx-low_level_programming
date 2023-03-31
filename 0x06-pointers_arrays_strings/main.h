915b1361919b1d26cd73b9fbbdac921c8c5c7499
Kana Azibabari Lagalo <kanaazibabari@gmail.com>
2023-03-30 19:33:36 -0700

    final pointers in c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,4 +6,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+char *rot13(char *);
 int _putchar(char c);
