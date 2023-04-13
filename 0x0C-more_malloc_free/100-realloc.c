d1b4e1f3c18d28483d47b4a643d6187d511bb058
Kana Azibabari Lagalo <kanaazibabari@gmail.com>
2023-04-13 09:01:18 -0700

    reallocate memory block

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *new_ptr;
+/* If new_size is zero, free memory and return NULL */
+if (new_size == 0)
+{
+free(ptr);
+return (NULL);
+}
+/* If ptr is NULL, allocate new memory */
+if (ptr == NULL)
+{
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+{
+return (NULL);
+}
+return (new_ptr);
+}
+/* If new_size is equal to old_size, do nothing and return ptr */
+if (new_size == old_size)
+{
+return (ptr);
+}
+/* Allocate new memory */
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+{
+return (NULL);
+}
+/* Copy contents of old memory block to new block */
+if (new_size > old_size)
+{
+memcpy(new_ptr, ptr, old_size);
+}
+else
+{
+memcpy(new_ptr, ptr, new_size);
+}
+/* Free old memory block */
+free(ptr);
+return (new_ptr);
+}
