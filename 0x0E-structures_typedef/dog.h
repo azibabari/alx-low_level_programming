48b4a234e541834d4c5048c4c9f21df0a0b52cc5
Kana Azibabari Lagalo <kanaazibabari@gmail.com>
2023-04-18 05:12:26 -0700

    initialize a variable

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,30 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
 
 /**
- * struct dog - a dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - dog class
  *
- * Description: a struct that contains information about a dog
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
+
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-/**
- * dog_t - Typedef for struct dog
- */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 typedef struct dog dog_t;
 
-#endif /* DOG_H */
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
