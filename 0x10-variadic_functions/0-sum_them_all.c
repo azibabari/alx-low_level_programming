489dfbaf0479c23856ce1554f27bdf0776b1c601
Kana Azibabari Lagalo <kanaazibabari@gmail.com>
2023-04-21 14:08:52 -0700

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,16 +7,14 @@
  **/
 int sum_them_all(const unsigned int n, ...)
 {
-    int sum = 0;
-    va_list args;
-    unsigned int i;
-
-    va_start(args, n);
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(args, int);
-    }
-    va_end(args);
-
-    return sum;
+int sum = 0;
+va_list args;
+unsigned int i;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+sum += va_arg(args, int);
+}
+va_end(args);
+return (sum);
 }
